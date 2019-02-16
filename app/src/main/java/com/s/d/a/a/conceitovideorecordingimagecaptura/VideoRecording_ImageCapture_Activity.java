02d9357a40ed2a22a42f3cd6d04e3c4875573a3f
Alexandre <aas@unochapeco.edu.br>
2019-02-16 10:40:06 -0200

    Codificação método iniciarGravação e outros

--- app/src/main/java/com/s/d/a/a/conceitovideorecordingimagecaptura/VideoRecording_ImageCapture_Activity.java
+++ app/src/main/java/com/s/d/a/a/conceitovideorecordingimagecaptura/VideoRecording_ImageCapture_Activity.java
@@ -1,11 +1,23 @@
 package com.s.d.a.a.conceitovideorecordingimagecaptura;
 
+import java.io.File;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.pm.PackageManager;
 import android.widget.Button;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 
 public class VideoRecording_ImageCapture_Activity extends AppCompatActivity {
+
+    private static final int VIDEO_CAPTURE = 87;
+    //private Uri fileUri;
+    //private static final String AUTORIDADE="com.s.d.a.a.conceitovideorecordingimagecaptura";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,30 @@ public class VideoRecording_ImageCapture_Activity extends AppCompatActivity {
 
     private boolean temCamera() {
         return (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY));
+    }
+
+    public void iniciarGravacao(View view)
+    {
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        startActivityForResult(intent, VIDEO_CAPTURE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Uri videoUri = data.getData();
+
+        if (requestCode == VIDEO_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Local do arquivo de vídeo:\n" +
+                        videoUri, Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Video recording cancelled.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Falha na gravação do vídeo",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 }
